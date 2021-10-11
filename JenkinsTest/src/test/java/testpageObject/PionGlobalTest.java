7e3db2ea2668fb31e9a46b51032a9ff8cbc0aa4a
Sonalijain263 <83583118+Sonalijain263@users.noreply.github.com>
2021-10-11 11:44:26 +0530

    Update PionGlobalTest.java

--- JenkinsTest/src/test/java/testpageObject/PionGlobalTest.java
+++ JenkinsTest/src/test/java/testpageObject/PionGlobalTest.java
@@ -25,7 +25,7 @@ public class PionGlobalTest {
   		PropertyConfigurator.configure("log4j.properties");
   		
 		WebDriverManager.chromedriver().setup();
-		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","JenkinsTest/chromedriver.exe");
 		 driver = new ChromeDriver();
 		 logger.info("Launching chrome browser");
 		 
